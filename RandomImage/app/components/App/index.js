c7dca758f243bfe2965a311a1f797df951705acf
Otto Grajeda <ottograjeda@gmail.com>
2019-07-25 11:51:31 -0700

    Update

--- RandomImage/app/components/App/index.js
+++ RandomImage/app/components/App/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import AppView from './AppView';
 
+console.disableYellowBox = true // disable warnings as needed (for demos)
+
 export default class AppContainer extends React.Component {
   render() {
       return (
